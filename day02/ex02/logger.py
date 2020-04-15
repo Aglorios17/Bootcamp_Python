e4525729d5d0682cf584c366558a4aed3c773ffb
Alessio Glorioso <alessioglorioso@MBP-de-Antonio.lan>
2020-04-15 19:07:36 +0200

    day02.2

--- day02/ex02/logger.py
+++ day02/ex02/logger.py
@@ -3,19 +3,18 @@ import getpass
 from random import randint
 
 def log(function):
-    user = getpass.getuser()
-    def inner(*args, **kwargs):
-        f = open("machine.log", "a+", encoding="utf8")
-        start = time.process_time()
-        function
-        total = time.process_time() - start
-        if (total > 0.001):
-            f.write(f"({user})Running: {function.__name__:30s} [ exec-time = {total:3f} s ]\n")
-        else:
-            f.write(f"({user})Running: {function.__name__:30s} [ exec-time = {total * 1000:3f} ms ]\n")
-        f.close()
-        return function(*args, **kwargs)
-    return inner
+	user = getpass.getuser()
+	def inner(*args, **kwargs):
+		start = time.process_time()
+		res = function(*args, **kwargs)
+		total = time.process_time() - start
+		with open("machine.log", "a+") as f:
+			if (total > 0.001):
+				f.write(f"({user})Running: {function.__name__:30s} [ exec-time = {total:3f} s ]\n")
+			else:
+				f.write(f"({user})Running: {function.__name__:30s} [ exec-time = {total * 1000:3f} ms ]\n")
+		return res
+	return inner
 
 class CoffeeMachine():
 
@@ -39,8 +38,8 @@ class CoffeeMachine():
 			for _ in range(20):
 				time.sleep(0.1)
 				self.water_level -= 1
-				print(self.boil_water())
-				print("Coffee is ready!")
+			print(self.boil_water())
+			print("Coffee is ready!")
 
 	@log
 	def add_water(self, water_level):
@@ -49,6 +48,7 @@ class CoffeeMachine():
 		print("Blub blub blub...")
 
 if __name__ == "__main__":
+
 	machine = CoffeeMachine()
 	for i in range(0, 5):
 		machine.make_coffee()
--- day02/ex03/csvreader.py
+++ day02/ex03/csvreader.py
@@ -1,3 +1,43 @@
+import csv
+
 class CsvReader():
-	def __init__(self, sep=',', header=False, skip_top=0, skip_bottom=0):
-		pass
+	def __init__(self, filename, sep=',', header=False, skip_top=0, skip_bottom=0):
+		self.filename = filename
+		self.sep = sep
+		self.header = None
+		self.header_bool = header
+		self.skip_top = skip_top
+		self.skip_bottom = skip_bottom
+
+	def __enter__(self):
+		self.fd = open(self.filename, 'r')
+		return self
+
+	def getheader(self):
+		if self.header is None:
+			self.header = self.fd.readline()
+		return self.header
+
+	def getdata(self):
+		self.getheader()
+		data = ""
+		if (self.header_bool):
+			data += self.fd.read()
+		if self.skip_bottom is 0:
+            		lst = self.fd.readlines()[self.skip_top:] # rend une liste de != chaine de charact [nombre:] passer le debut
+		else:
+			lst = self.fd.readlines()[self.skip_top:-self.skip_bottom] # :- = enlever ...
+		for s in lst:
+			data += s
+		return data
+		
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		self.fd.close()
+
+if( __name__ == "__main__" ):
+	with CsvReader('good.csv') as file:
+		data = file.getdata()
+		print (data)
+		print ('\n')
+		header = file.getheader()
+		print (header)
